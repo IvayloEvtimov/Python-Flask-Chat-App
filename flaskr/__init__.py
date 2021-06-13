ba2e6ac2759f7694d09f34e120fc31b8e5a3c1d9
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-06-13 16:15:02 +0300

    add chat endpoint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from . import chat
 from . import auth
 from . import db
 
@@ -29,5 +30,8 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(auth.bp)
+
+    app.register_blueprint(chat.bp)
+    app.add_url_rule("/", endpoint="index")
 
     return app
