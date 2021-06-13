50e36d95ee48fa6287593d91a0317b0fad9a9b59
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-06-13 22:38:19 +0300

    auth.py: Fix sql query

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -30,7 +30,9 @@ def register():
         elif not password:
             error = "Password is required."
         elif (
-            db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone()
+            db.execute(
+                "SELECT username FROM user WHERE username = ?", (username,)
+            ).fetchone()
             is not None
         ):
             error = f"User {username} is already registered."
@@ -80,7 +82,11 @@ def load_logged_in_user():
     if user is None:
         g.user = None
     else:
-        g.user = get_db().execute("SELECT * FROM user WHERE id = ?", (user,)).fetchone()
+        g.user = (
+            get_db()
+            .execute("SELECT * FROM user WHERE username = ?", (user,))
+            .fetchone()
+        )
 
 
 @bp.route("/logout")
