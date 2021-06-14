99f32786394a3926d754ed184117e58c54fca4fa
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-06-15 00:01:19 +0300

    schema.sql: Add chat table

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -6,10 +6,16 @@
 -- );
 
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS person_chat;
 
 CREATE TABLE user (
 --   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT PRIMARY KEY NOT NULL,
   password TEXT NOT NULL
+);
+
+CREATE TABLE person_chat (
+    recipient1 TEXT PRIMARY KEY NOT NULL,
+    recipient2 TEXT PRIMARY KEY NOT NULL,
+    chat_file TEXT NOT NULL
 );
