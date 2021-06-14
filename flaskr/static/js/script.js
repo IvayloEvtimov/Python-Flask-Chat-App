24a4e65d0c559dc4a900d64fc6a79ad390670919
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-06-14 23:41:03 +0300

    script.js: Add contact search

--- flaskr/static/js/script.js
+++ flaskr/static/js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
     $("#search-text").keyup(function () {
-        console.log("KEY");
+        // console.log("KEY");
         $.ajax({
             url: "/search",
             method: "POST",
             data: { username: $(this).val() },
             success: function (data) {
-                $("#search-result").html(data);
+                var obj = JSON.parse(data);
+                var string = "";
+
+                if (obj.length > 0) {
+                    for (var count = 0; count < obj.length; ++count) {
+                        string = string.concat("<li class='clearfix'>\n\t<img src = 'https://bootdey.com/img/Content/avatar/avatar2.png' alt = 'avatar'>\n\t<div class='about'>\n\t\t<div class='name'>" + obj[0].username + "</div>\n\t</div>\n</li>\n")
+                    }
+                }
+
+                $(".chat-list").html(string);
             }
         });
     });
