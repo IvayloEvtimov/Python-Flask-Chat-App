8057ba89b84aaecbdd738725007d3f743d38ceac
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2021-06-15 09:22:38 +0300

    chat.py: Add chat loading

--- flaskr/chat.py
+++ flaskr/chat.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask.globals import session
+from flask.helpers import send_from_directory
 from werkzeug.exceptions import abort
+from werkzeug.utils import secure_filename
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+import random
+import string
 import json
+import os
 
 bp = Blueprint("chat", __name__)
 
@@ -29,4 +35,33 @@ def searchContact():
     if len(users) == 0:
         return json.dumps([])
 
+    return json.dumps([dict(user) for user in users])
 
+
+@bp.route("/loadChat", methods=["POST"])
+def loadChat():
+    db = get_db()
+
+    recipient1 = request.form["recipient"]
+    recipient2 = session["username"]
+
+    chat_file_path = db.execute(
+        "SELECT chat_file FROM person_chat WHERE (recipient1 = ? AND recipient2 = ?) OR (recipient2 = ? AND recipient1 = ?)",
+        (recipient1, recipient2, recipient1, recipient2),
+    ).fetchone()
+
+    if chat_file_path is None:
+        chat_file_path = string.ascii_letters
+        chat_file_path = "".join(random.choice(chat_file_path) for i in range(10))
+        db.execute(
+            "INSERT INTO person_chat (recipient1,recipient2,chat_file) VALUES  (?,?,?)",
+            (recipient1, recipient2, chat_file_path),
+        )
+        db.commit()
+        return json.dumps([])
+
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/chats", chat_file_path[0])
+    data = json.load(open(json_url))
+
+    return data
