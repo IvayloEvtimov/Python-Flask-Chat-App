69ba0a564e63893deeec9d4ecde3554db1af6a12
Ivo Evtimov <ivoevtimov97@abv.bg>
2022-03-13 20:33:45 +0200

    Update schema.sql

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -7,11 +7,14 @@
 
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS person_chat;
+DROP TABLE IF EXISTS group_chat;
+DROP TABLE IF EXISTS group_participants;
 
 CREATE TABLE user (
 --   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT PRIMARY KEY NOT NULL,
-  password TEXT NOT NULL
+    username TEXT PRIMARY KEY NOT NULL,
+    avatar TEXT,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE person_chat (
@@ -20,3 +23,16 @@ CREATE TABLE person_chat (
     chat_file TEXT NOT NULL,
     PRIMARY KEY(recipient1, recipient2)
 );
+
+-- CREATE TABLE group_chat (
+--     chat_file TEXT PRIMARY KEY NOT NULL,
+--     group_name TEXT NOT NULL
+-- );
+
+-- CREATE TABLE group_participants (
+--     user TEXT NOT NULL,
+--     group_chat TEXT NOT NULL,
+--     FOREIGN KEY(user) REFERENCES user(username),
+--     FOREIGN KEY(group_chat) REFERENCES group_chat(chat_file),
+--     PRIMARY KEY(user, group_chat)
+-- );
